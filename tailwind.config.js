323e318506f1143bb76e3b7976634004f1b0f63b
Matheus Pereira <matheussbp2014@gmail.com>
2023-08-08 19:33:35 -0300

    Add extends styles in tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        tomato: 'hsl(4, 100%, 67%)',
+        darkSlateGray: 'hsl(234, 29%, 20%)',
+        charcoalGrey: 'hsl(235, 18%, 26%)',
+        grey: 'hsl(231, 7%, 60%)'
+      },
+      screens: {
+        sm: {max: '375px'}
+      }
     },
   },
   plugins: [],
